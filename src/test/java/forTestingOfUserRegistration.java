34e4f264d9d1b4e525ee999adf93c705ae4f1cb8
Amit <amitlpalandurkar1998@gmail.com>
2023-07-12 21:53:56 +0530

    Test caseses for multiple Email ID's.

--- src/test/java/forTestingOfUserRegistration.java
+++ src/test/java/forTestingOfUserRegistration.java
@@ -6,9 +6,18 @@ public class forTestingOfUserRegistration {
     void testCase(){
         Assertions.assertEquals("Valid",UserRegisterationTesting.getUserFirstName("Amit"));
         Assertions.assertEquals("Valid",UserRegisterationTesting.getUserLastName("Palandurkar"));
-        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("amit123.amit@co.co.in"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("amit123.amit@gmail.com"));
         Assertions.assertEquals("Valid",UserRegisterationTesting.getUserPhoneNumber("918888879983"));
         Assertions.assertEquals("Valid",UserRegisterationTesting.getUserPassword("Doremon@1234"));
-    }
 
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc@yahoo.com"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc-100@yahoo.com"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc.100@yahoo.com"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc111@abc.com"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc-100@abc.net"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc.100@abc.com.au"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc@1.com"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc@gmail.com"));
+        Assertions.assertEquals("Valid",UserRegisterationTesting.getUserEmailID("abc+100@gmail.com"));
+    }
 }
